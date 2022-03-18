3ff955e67cbe5e18a431a3a0eaa254143fe04244
Bruno Hubner <brunohubnerdev@gmail.com>
2022-03-18 16:29:33 -0300

    feat: create search posts

--- posts/views.py
+++ posts/views.py
@@ -2,22 +2,55 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from posts.models import Post
+from django.db.models import Q, Count, Case, When
 
 
 class PostIndex(ListView):
     model = Post
     template_name = 'posts/index.html'
-    paginate_by = 3
+    paginate_by = 6
     context_object_name = 'posts'
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.order_by('-id').filter(publicated=True)
+        qs = qs.annotate(
+            commnents_count=Count(
+                Case(When(comment__publicated=True, then=1))
+            )
+        )
+        return qs
 
-class PostSearch(ListView):
-    pass
+
+class PostSearch(PostIndex):
+    template_name = 'posts/post_search.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        search = self.request.GET.get('q')
+        if not search:
+            return qs
+        qs = qs.filter(
+            Q(title__icontains=search) |
+            Q(author__first_name__iexact=search) |
+            Q(content__icontains=search) |
+            Q(excerpt__icontains=search) |
+            Q(category__name__iexact=search)
+        )
+        return qs
 
 
-class PostCategory(ListView):
-    pass
+class PostCategory(PostIndex):
+    template_name = 'posts/post_category.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        category = self.kwargs.get('category', None)
+        if not category:
+            return qs
+        qs = qs.filter(category__name__iexact=category)
+        return qs
 
 
-class PostDetails(ListView):
+class PostDetails(UpdateView):
     pass
